fdfd7c0071dfb62811b321590917efb8a1bbbe83
Stephen Prill <stephen.prill@gmail.com>
2015-01-30 00:08:41 -0700

    altered app.js and app.css

--- javascript/app.js
+++ javascript/app.js
@@ -1,23 +1,31 @@
 "use strict";
 
-$('.container').on('click','.fa-play', function() {
-$('.fa').removeClass('fa-pause').addClass('fa-play');
-$(this).removeClass('fa-play').addClass('fa-pause');
-})
-
-$('.container').on('click', '.fa-pause', function(){
-$(this).removeClass('fa-pause').addClass('fa-play');
-})
+// $('.container').on('click','.fa-play', function() {
+// $('.fa').removeClass('fa-pause').addClass('fa-play');
+// $(this).removeClass('fa-play').addClass('fa-pause');
+// })
+//
+// $('.container').on('click', '.fa-pause', function(){
+// $(this).removeClass('fa-pause').addClass('fa-play');
+// })
 
 
 $(document).ready(function () {
 
-  $.getJSON('posts.json', function(data) {
-    var $postsTemplate = $('#postsTemplate').html();
+  $.getJSON('data.json', function(data) {
+    var $playerTemplate = $('#trackTemplate').html();
 
-    var newHTML = Mustache.to_html($postsTemplate, data);
+    data.tracks.forEach(function (track) {
+      var newHTML = Mustache.to_html($playerTemplate, track);
 
-    $('.posts').html(newHTML);
+      $('#tracksList').append(newHTML);
+    });
+
+    $('.control').on('click', function () {
+      $(this).find('i').toggleClass('hidden')
+    });
+
+
   });
 
 });
